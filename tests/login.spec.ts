1c5b8e19dbe4f15353c8c9a58f5fdcf3daaee2b5
Audinga.Cijunaityte <audingacij@gmail.com>
2025-10-27 20:34:52 +0200

    ran prettier

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,13 +1,12 @@
 import { expect, test } from '@playwright/test'
 import { LoginDto } from '../dto/login-dto'
-import {StatusCodes} from 'http-status-codes'
+import { StatusCodes } from 'http-status-codes'
 
 test('should return token with correct username and password', async ({ request }) => {
   // prepare request body
-  const requestBody =  LoginDto.createLoginDto()
-  const response = await request.post('https://backend.tallinn-learning.ee/login/student',
-  {
-    data: requestBody
+  const requestBody = LoginDto.createLoginDto()
+  const response = await request.post('https://backend.tallinn-learning.ee/login/student', {
+    data: requestBody,
   })
   console.log('response body and token:', await response.text())
   expect(response.status()).toBe(StatusCodes.OK)
@@ -16,10 +15,9 @@ test('should return token with correct username and password', async ({ request 
 test('should not return token with incorrect username and password', async ({ request }) => {
   // prepare request body
   const requestBody = new LoginDto('audingac', '')
-  const response = await request.post('https://backend.tallinn-learning.ee/login/student',
-    {
-      data: requestBody
-    })
+  const response = await request.post('https://backend.tallinn-learning.ee/login/student', {
+    data: requestBody,
+  })
   console.log('response body and token:', await response.text())
   expect(response.status()).toBe(StatusCodes.UNAUTHORIZED)
 })
